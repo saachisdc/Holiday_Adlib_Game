db39cb31a8dedf2ec4d22bed00be224af066e1fd
saachisdc <128309641+saachisdc@users.noreply.github.com>
2025-12-06 14:38:05 -0500

    deleted section wraps

--- src/App.jsx
+++ src/App.jsx
@@ -10,36 +10,31 @@ export default function App() {
   return (
     <Router>
       <div className="container">
-        <section>
-          <h1> Baked Mittens</h1>
-        </section>
-        <section>
-          <p>
-            'White lakes swirled around in the landscape behind me, accumulating
-            in the corners of the bakery window, as I ordered a hot
-            <strong> cup of coffee </strong>
-            with a sweet treat. The{" "}
-            <emphasis>
-              latte swirl melted into the surface while a puff of steam curled
-              on top
-            </emphasis>
-            . I snagged a table by the window, shucked off my{" "}
-            <strong> mittens </strong> next to my plate. My defrosting fingers
-            smelled lightly of{" "}
-            <emphasis>lanolin, which made my nose wrinkle</emphasis>. Sitting
-            back, I contentedly took a big bite out of my{" "}
-            <strong> donut </strong>
-            <emphasis>
-              and let the sweet glaze mingle with the bitter caffeine already
-              coating my stomach
-            </emphasis>
-            . I absently watched the snowflakes dance outside while{" "}
-            <emphasis>
-              listening to the bustling hum of other patrons munching away
-            </emphasis>
-            . What a <emphasis>delightfully cozy</emphasis> day.'
-          </p>
-        </section>
+        <h1> Baked Mittens</h1>
+        <p>
+          'White lakes swirled around in the landscape behind me, accumulating
+          in the corners of the bakery window, as I ordered a hot
+          <strong> cup of coffee </strong>
+          with a sweet treat. The{" "}
+          <emphasis>
+            latte swirl melted into the surface while a puff of steam curled on
+            top
+          </emphasis>
+          . I snagged a table by the window, shucked off my{" "}
+          <strong> mittens </strong> next to my plate. My defrosting fingers
+          smelled lightly of{" "}
+          <emphasis>lanolin, which made my nose wrinkle</emphasis>. Sitting
+          back, I contentedly took a big bite out of my <strong> donut </strong>
+          <emphasis>
+            and let the sweet glaze mingle with the bitter caffeine already
+            coating my stomach
+          </emphasis>
+          . I absently watched the snowflakes dance outside while{" "}
+          <emphasis>
+            listening to the bustling hum of other patrons munching away
+          </emphasis>
+          . What a <emphasis>delightfully cozy</emphasis> day.'
+        </p>
         <button class="btn"> Icon 1 </button>
         <button class="btn"> Icon 2 </button>
         <button class="btn"> Icon 3 </button>
